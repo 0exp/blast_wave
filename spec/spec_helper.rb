bc7a7aa55e1bd3adafd8a790b9a2a250a61b4ca3
Rustam Ibragimov <iamdaiver@icloud.com>
2018-08-12 16:53:05 +0300

    [testing] spec env

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,25 +1,29 @@
 # frozen_string_literal: true
 
 require 'simplecov'
-require 'simplecov-json'
 require 'coveralls'
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::HTMLFormatter,
-  SimpleCov::Formatter::JSONFormatter,
   Coveralls::SimpleCov::Formatter
 ])
 
 SimpleCov.start { add_filter 'spec' }
 
 require 'bundler/setup'
-require 'blastwave'
+require 'blast_wave'
 require 'pry'
 
+require_relative 'support/fake_app'
+require_relative 'support/request_helpers'
+
 RSpec.configure do |config|
-  config.disable_monkey_patching!
   config.shared_context_metadata_behavior = :apply_to_host_groups
   config.expect_with(:rspec) { |c| c.syntax = :expect }
   config.order = :random
   Kernel.srand config.seed
+
+  config.include Rack::Test::Methods
+  config.include SpecSupport::FakeApp
+  config.include SpecSupport::RequestHelpers
 end
